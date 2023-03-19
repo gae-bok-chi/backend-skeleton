b8aa744c8a7daa5eef17b7244c476f65c1699088
shinilseop <shinilseop12@gmail.com>
2023-03-19 22:22:49 +0900

    GBCH-11 init project

---
+++ src/main/java/com/gaebokchi/backendskeleton/BackendSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.gaebokchi.backendskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSkeletonApplication.class, args);
+	}
+
+}
